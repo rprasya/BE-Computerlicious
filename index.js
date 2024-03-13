d7cc0defcc8c39c57956b156c5c7b28d74532d6b
Mahsa Nurfarhan Hidayat <nurfarhan52@gmail.com>
2024-03-13 13:52:33 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
+// First to do, import library
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+// Callback <> biar Req App express kepake
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Create response for success route
 app.get("/", async (req, res) => {
-    res.send("here is the response")
+  res.send("Routes success");
 });
 
+// Create response for error or missing route
 app.all("*", async (req, res) => {
-    res.json({
-        message: "Routes you're looking is not found"
-    });
+  res.json({
+    message: "Routes you're looking is not found",
+  });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is already running at ${PORT}`)
-})
-
+// Create response for route
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is already running at ${PORT}`);
+});
